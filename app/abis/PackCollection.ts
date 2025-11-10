816a301c35fdeb035fed3ac979137a571507a9d1
Ken <111918257+kennndev@users.noreply.github.com>
2025-11-10 18:49:02 +0500

    Update PackCollection.ts

--- app/abis/PackCollection.ts
+++ app/abis/PackCollection.ts
@@ -1,12 +1,15 @@
 export const PACK_COLLECTION_ABI = [
-    {"type":"function","name":"mintPacks","stateMutability":"payable","inputs":[{"name":"amount","type":"uint256"}],"outputs":[]},
-    {"type":"function","name":"openPack","stateMutability":"nonpayable","inputs":[{"name":"amount","type":"uint256"}],"outputs":[]},
-    {"type":"function","name":"uri","stateMutability":"view","inputs":[{"name":"id","type":"uint256"}],"outputs":[{"type":"string"}]},
-    {"type":"function","name":"balanceOf","stateMutability":"view","inputs":[{"name":"account","type":"address"},{"name":"id","type":"uint256"}],"outputs":[{"type":"uint256"}]},
-    {"type":"function","name":"packPrice","stateMutability":"view","inputs":[],"outputs":[{"type":"uint256"}]},
-    {"type":"function","name":"maxPacks","stateMutability":"view","inputs":[],"outputs":[{"type":"uint256"}]},
-    {"type":"function","name":"packsMinted","stateMutability":"view","inputs":[],"outputs":[{"type":"uint256"}]},
-    {"type":"function","name":"name","stateMutability":"view","inputs":[],"outputs":[{"type":"string"}]},
-    {"type":"function","name":"symbol","stateMutability":"view","inputs":[],"outputs":[{"type":"string"}]},
-  ] as const
-  
+  { "type": "function", "name": "mintPacks", "stateMutability": "payable", "inputs": [{ "name": "amount", "type": "uint256" }], "outputs": [] },
+  { "type": "function", "name": "openPack", "stateMutability": "nonpayable", "inputs": [{ "name": "amount", "type": "uint256" }], "outputs": [] },
+  { "type": "function", "name": "uri", "stateMutability": "view", "inputs": [{ "name": "id", "type": "uint256" }], "outputs": [{ "type": "string" }] },
+  { "type": "function", "name": "balanceOf", "stateMutability": "view", "inputs": [{ "name": "account", "type": "address" }, { "name": "id", "type": "uint256" }], "outputs": [{ "type": "uint256" }] },
+  { "type": "function", "name": "packPrice", "stateMutability": "view", "inputs": [], "outputs": [{ "type": "uint256" }] },
+  { "type": "function", "name": "maxPacks", "stateMutability": "view", "inputs": [], "outputs": [{ "type": "uint256" }] },
+  { "type": "function", "name": "packsMinted", "stateMutability": "view", "inputs": [], "outputs": [{ "type": "uint256" }] },
+  { "type": "function", "name": "name", "stateMutability": "view", "inputs": [], "outputs": [{ "type": "string" }] },
+  { "type": "function", "name": "symbol", "stateMutability": "view", "inputs": [], "outputs": [{ "type": "string" }] },
+  // Optional admin functions (for creator dashboards)
+  { "type": "function", "name": "setTokenURI", "stateMutability": "nonpayable", "inputs": [{ "name": "id", "type": "uint256" }, { "name": "newURI", "type": "string" }], "outputs": [] },
+  { "type": "function", "name": "freezeMetadata", "stateMutability": "nonpayable", "inputs": [], "outputs": [] },
+  { "type": "function", "name": "setMarketplace", "stateMutability": "nonpayable", "inputs": [{ "name": "mp", "type": "address" }], "outputs": [] }
+] as const
